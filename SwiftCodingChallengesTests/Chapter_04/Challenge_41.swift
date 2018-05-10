d7b245a16ee5c4ecf0b90c051303c1c844e6e8e6
Erick Andrade <erickandrade21@gmail.com>
2018-05-09 23:02:04 -0700

    Challenge 41

---
+++ SwiftCodingChallengesTests/Chapter_04/Challenge_41.swift
@@ -0,0 +1,40 @@
+//
+//  Challenge_41.swift
+//  SwiftCodingChallengesTests
+//
+//  Created by Erick Andrade on 5/9/18.
+//
+
+import XCTest
+
+/// Write an extension to collections that accepts an array of Int and returns the median average, or nil if there are no values.
+
+extension Collection where Element == Int{
+
+    func median() -> Double? {
+        guard !isEmpty else {
+            return nil
+        }
+
+        let sorted = self.sorted()
+        let midIndex = sorted.count / 2
+        if (sorted.count % 2 == 0) {
+            let midLhs = sorted[midIndex - 1]
+            let midRhs = sorted[midIndex]
+            return Double(midLhs) + (Double(midRhs - midLhs) / 2.0)
+        } else {
+            return Double(sorted[midIndex])
+        }
+    }
+}
+
+class Challenge_41: XCTestCase {
+
+    func test01() {
+        XCTAssertEqual([1,2,3].median(), 2)
+        XCTAssertEqual([1,2,9].median(), 2)
+        XCTAssertEqual([1,3,5,7,9].median(), 5)
+        XCTAssertEqual([1,2,3,4].median(), 2.5)
+        XCTAssertEqual([Int]().median(), nil)
+    }
+}
