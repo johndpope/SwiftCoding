abd0a62706f259e0c85e7466574d78409f4db1e1
Erick Andrade <erickandrade21@gmail.com>
2018-05-17 23:54:36 -0700

    Challenge 47 - Append

--- SwiftCodingChallengesTests/Chapter_04/Challenge_47.swift
+++ SwiftCodingChallengesTests/Chapter_04/Challenge_47.swift
@@ -18,6 +18,25 @@ private extension Collection where Element: Comparable {
             }
         }
     }
+
+    func min3() -> Element? {
+        var iterator = makeIterator()
+        guard var min = iterator.next() else { return nil }
+
+        while let item = iterator.next() {
+            if item < min {
+                min = item
+            }
+        }
+
+        return min
+    }
+
+    func min4() -> Element? {
+        var iterator = makeIterator()
+        guard let min = iterator.next() else { return nil }
+        return reduce(min) { $1 < $0 ? $1 : $0 }
+    }
 }
 
 class Challenge_47: XCTestCase {
@@ -27,5 +46,16 @@ class Challenge_47: XCTestCase {
         XCTAssertEqual(["q", "f", "k"].min2(), "f")
         XCTAssertEqual([4096, 256, 16].min2(), 16)
         XCTAssertEqual([String]().min2(), nil)
+
+        XCTAssertEqual([1,2,3].min3(), 1)
+        XCTAssertEqual(["q", "f", "k"].min3(), "f")
+        XCTAssertEqual([4096, 256, 16].min3(), 16)
+        XCTAssertEqual([String]().min3(), nil)
+
+        XCTAssertEqual([1,2,3].min4(), 1)
+        XCTAssertEqual(["q", "f", "k"].min4(), "f")
+        XCTAssertEqual([4096, 256, 16].min4(), 16)
+        XCTAssertEqual([String]().min4(), nil)
+
     }
 }
