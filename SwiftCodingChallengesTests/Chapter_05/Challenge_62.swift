c129116c0857d53048ee1885a2329445a5e55e96
Erick Andrade <erickandrade21@gmail.com>
2018-07-17 21:12:29 -0700

    Challenge 62

--- SwiftCodingChallengesTests/Chapter_05/Challenge_62.swift
+++ SwiftCodingChallengesTests/Chapter_05/Challenge_62.swift
@@ -7,42 +7,54 @@
 
 import XCTest
 
-private func angles(points: [CGPoint]) -> [Double] {
-    let (opposite, adjacent) = slope(points[0], points[1])
-    let _baseAngle = baseAngle(opposite, adjacent)
-    let _angle = angle(_baseAngle, opposite, adjacent)
-    return [_angle]
+private func angles(points: [(CGPoint, CGPoint)]) -> [Double] {
+    return points.map { pointA, pointB -> Double in
+        let (opposite, adjacent) = slope(pointA, pointB)
+        let _baseAngle = baseAngle(opposite, adjacent)
+        return angle(_baseAngle, opposite, adjacent)
+    }
 }
 
 private func slope(_ pointA: CGPoint, _ pointB: CGPoint) -> (opposite: Double, adjacent: Double) {
-    let opposite = pointB.y - pointB.y
-    let adjacent = pointB.x - pointB.x
+    let opposite = pointA.y - pointB.y
+    let adjacent = pointA.x - pointB.x
 
     return (opposite: Double(opposite), adjacent: Double(adjacent))
 }
 
 private func baseAngle(_ opposite: Double, _ adjacent: Double) -> Double {
-    guard adjacent != 0 else { return 0.0 }
-    return atan(opposite / adjacent) * 180.0 / Double.pi
+    return (atan2(opposite, adjacent) * 180.0 / Double.pi) - 90.0
 }
 
 private func angle(_ baseAngle: Double, _ opposite: Double, _ adjacent: Double) -> Double {
-    switch (opposite >= 0, adjacent >= 0) {
-    case (true, true):
-        return baseAngle
-    case (false, true):
-        return 90.0 + baseAngle
-    case (false, false):
-        return 180.0 + baseAngle
-    case (true, false):
-        return 270 + baseAngle
-    }
+    var result: Double = baseAngle
+    if result < 0 { result += 360.0 }
+    if result == 360.0 { result = 0.0 }
+    return result
 }
 
 class Challenge_62: XCTestCase {
 
     func test() {
-        let angle = angles(points: [CGPoint.zero, CGPoint(x: 0, y: -100)])
-        XCTAssertEqual(angle, [0.0])
+        let points = [
+            (CGPoint.zero, CGPoint(x: 0, y: -100)),
+            (CGPoint.zero, CGPoint(x: 100, y: -100)),
+            (CGPoint.zero, CGPoint(x: 100, y: 0)),
+            (CGPoint.zero, CGPoint(x: 100, y: 100)),
+            (CGPoint.zero, CGPoint(x: 0, y: 100)),
+            (CGPoint.zero, CGPoint(x: -100, y: 100)),
+            (CGPoint.zero, CGPoint(x: -100, y: 0)),
+            (CGPoint.zero, CGPoint(x: -100, y: -100)),
+        ]
+        let pointsToAngles = angles(points: points)
+        XCTAssertEqual(pointsToAngles, [0.0, 45.0, 90.0, 135.0, 180.0, 225.0, 270.0, 315.0])
+    }
+
+    func test2() {
+        let points = [
+            (CGPoint.zero, CGPoint(x: 100, y: -100)),
+            ]
+        let pointsToAngles = angles(points: points)
+        XCTAssertEqual(pointsToAngles, [45.0])
     }
 }
